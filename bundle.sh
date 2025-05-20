03da05220d9da3b3f905bf4689a5f31c5aff5eff
arrowpc <battle.beater2005@gmail.com>
2025-05-20 14:55:51 +1000

    Add icon and bundle script

---
+++ bundle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+APP_NAME="HR"
+BUILD_PATH=".build/release/$APP_NAME"
+ASSETS_PATH="assets"
+APP_BUNDLE="$APP_NAME.app"
+
+swift build -c release
+
+rm -rf "$APP_BUNDLE"
+mkdir -p "$APP_BUNDLE/Contents/MacOS"
+mkdir -p "$APP_BUNDLE/Contents/Resources"
+
+cp "$BUILD_PATH" "$APP_BUNDLE/Contents/MacOS/"
+
+cp Info.plist "$APP_BUNDLE/Contents/"
+
+cp "$ASSETS_PATH/HR.icns" "$APP_BUNDLE/Contents/Resources/"
+
+echo "App bundle created at $APP_BUNDLE"
